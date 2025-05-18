2e577bc25d3b46756fbff5ccea32f21b9b953aa7
maryam <eramy.mary@gmail.com>
2025-05-18 18:09:28 +0330

    compplecting the authentication for sign in

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,11 @@
 import 'package:email_validator/email_validator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat/components/rounded_button.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+
+import 'chat_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'login_screen';
@@ -12,8 +16,15 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  bool obscureText = true;
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  var auth = FirebaseAuth.instance;
+  String errorMessage = '';
+  bool errorOccurred = false, showSpinner = false;
 
+
+  bool obscureText = true;
   void togglePasswordVisibility() {
     setState(() {
       obscureText = !obscureText;
@@ -24,64 +35,112 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black12,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: SizedBox(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
-              ),
-            ),
-            SizedBox(height: 48.0),
-            TextFormField(
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your email',
-                labelText: 'Email',
-              ),
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              validator: (email) {
-                return email != null && EmailValidator.validate(email)
-                    ? null
-                    : 'Please enter a valid email';
-              },
-              onChanged: (value) {},
-            ),
-            SizedBox(height: 8),
-            TextFormField(
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your password',
-                labelText: 'Password',
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    togglePasswordVisibility();
-                  },
-                  icon: Icon(
-                    obscureText ? Icons.visibility_off : Icons.visibility,
-                  ),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: SizedBox(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
                 ),
               ),
-              //pass security
-              obscureText: true,
-              //pass validation
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              validator: (password) {
-                return password != null && password.length > 5
-                    ? null
-                    : 'The password should be of 6 character at least';
-              },
-            ),
-            SizedBox(height: 24.0),
-            RoundedButton(
-              title: 'LogIn',
-              color: Colors.purple[200]!,
-              onPressed: () {},
-            ),
-          ],
+              SizedBox(height: 48.0),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: kTextFieldDecoration.copyWith(
+                        hintText: 'Enter your email',
+                        labelText: 'Email',
+                      ),
+                      controller: _emailController,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (email) {
+                        return email != null && EmailValidator.validate(email)
+                            ? null
+                            : 'Please enter a valid email';
+                      },
+                    ),
+                    SizedBox(height: 8),
+                    TextFormField(
+                      decoration: kTextFieldDecoration.copyWith(
+                        hintText: 'Enter your password',
+                        labelText: 'Password',
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            togglePasswordVisibility();
+                          },
+                          icon: Icon(
+                            obscureText ? Icons.visibility_off : Icons.visibility,
+                          ),
+                        ),
+                      ),
+                      //pass security
+                      obscureText: true,
+                      controller: _passwordController,
+                      //pass validation
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (password) {
+                        return password != null && password.length > 5
+                            ? null
+                            : 'The password should be of 6 character at least';
+                      },
+                    ),
+                  ]
+                ),
+              ),
+              SizedBox(height: 8),
+              Visibility(
+                visible: errorOccurred,
+                child: Text(
+                  errorMessage,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.red, fontSize: 14),
+                ),
+              ),
+              SizedBox(height: 24.0),
+              RoundedButton(
+                title: 'LogIn',
+                color: Colors.purple[200]!,
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    try {
+                      setState(() {
+                        errorOccurred = false;
+                        showSpinner = true;
+                      });
+                      await auth
+                          .signInWithEmailAndPassword(
+                        email: _emailController.text,
+                        password: _passwordController.text,
+                      )
+                          .then((Value) {
+                        Navigator.pop(context);
+                        Navigator.pushNamed(context, ChatScreen.id);
+                      });
+                      setState(() {
+                        showSpinner = false;
+                      });
+                    } catch (e) {
+                      setState(() {
+                        showSpinner = false;
+                        errorOccurred = true;
+                        errorMessage = e.toString().split('] ')[1];
+                      });
+                    }
+                  }
+                },
+
+              ),
+            ],
+          ),
         ),
       ),
     );
